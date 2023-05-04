4f29a34d371325015f7045f4f4f0a55d96ac4aed
lrao01-su <lrao01@syr.edu>
2023-05-03 22:37:02 -0400

    Create final_project.py

---
+++ final_project.py
@@ -0,0 +1,12 @@
+import streamlit as st                  # pip install streamlit
+
+st.markdown("# Practical Applications of Machine Learning (PAML)")
+
+#############################################
+st.sidebar.image: st.sidebar.image("/Users/jinglin/Desktop/PAML/Uber_Lyft_Final/datasets/uberlyft_logo.png", use_column_width=True)
+
+#############################################
+
+st.markdown("Add some description about your project, team, etc")
+
+st.markdown("Click **Explore Dataset** to get started.")

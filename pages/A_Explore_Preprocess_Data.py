c3c48afa9291c7b3453994ca762a3d86eee45331
lrao01-su <lrao01@syr.edu>
2023-05-10 17:17:06 -0400

    Update A_Explore_Preprocess_Data.py

--- pages/A_Explore_Preprocess_Data.py
+++ pages/A_Explore_Preprocess_Data.py
@@ -1,5 +1,5 @@
 import streamlit as st                  # pip install streamlit
-from helper_functions import fetch_dataset, display_missingValue
+from helper_functions import load_dataset, display_missingValue
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -28,18 +28,29 @@ st.markdown("### Farelytics ML: Using Machine Learning to Analyze and Compare Ub
 st.markdown('# Explore & Preprocess Dataset')
 
 #############################################
-df = None
-df = fetch_dataset()
+col1, col2 = st.columns(2)
+# with(col1):
+with(col1):
+    cab_data = st.file_uploader("Upload Your df_cab Dataset", type=['csv','txt'])
+# with(col2): #upload from cloud
+with(col2):
+    weather_data = st.file_uploader("Upload Your df_weather Dataset", type=['csv','txt'])
+if cab_data and weather_data:
+    ###################### EXPLORE DATASET #######################
+    st.markdown('### Explore Dataset Features')
 
-
-
-if df is not None:
-    # Display original dataframe
-    st.markdown('View initial data with missing values or invalid inputs')
-    st.markdown('You have uploaded the dataset.')
-    #write multiple dataframes
-    for i in range(len(df)):
-        st.dataframe(df[i])
+    # Load dataset
+    df_cab = load_dataset(cab_data)
+    df_weather = load_dataset(weather_data)
+    # Restore dataset if already in memory
+    st.session_state['df_cab']=df_cab
+    st.session_state['df_weather']=df_weather
+    
+    # Display dataframe as table using streamlit dataframe function
+    st.write("df_cab")
+    st.dataframe(df_cab)
+    st.write("df_weather")
+    st.dataframe(df_weather)
 
     # Inspect the dataset
     st.markdown('### Inspect and visualize some interesting features')
@@ -63,6 +74,6 @@ if df is not None:
 
 
     st.markdown('### You have preprocessed the dataset.')
-    st.dataframe(df)
+    #st.dataframe(df)
 
     st.write('Continue to Train Model')

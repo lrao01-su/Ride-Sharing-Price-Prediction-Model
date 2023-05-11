01033acfe8a5afbbc71e235e20b4d11c7dd5f9a3
acuna222 <123598414+acuna222@users.noreply.github.com>
2023-05-11 16:01:19 -0400

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -23,6 +23,27 @@ def display_missingValue(df_cab, df_weather):
     st.write(df_weather.isnull().sum())
     return df_cab.isnull().sum(), df_weather.isnull().sum()
 
+def remove_outliers(df, feature):
+    """
+    This function removes the outliers of the given feature(s)
+
+    Input: 
+        - df: pandas dataframe
+        - feature: the feature(s) to remove outliers
+    Output: 
+        - dataset: the updated data that has outliers removed
+        - lower_bound: the lower 25th percentile of the data
+        - upper_bound: the upper 25th percentile of the data
+    """
+    dataset, lower_bound, upper_bound = None, -1, -1
+
+    # Add code here
+
+    st.write('remove_outliers not implemented yet.')
+    return dataset, lower_bound, upper_bound
+
+
+
 
 
 

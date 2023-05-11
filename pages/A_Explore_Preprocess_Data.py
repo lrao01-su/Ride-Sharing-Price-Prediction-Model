86e10a2e5db43ee27997a7a0f7aaeb2b73b24394
lrao01-su <lrao01@syr.edu>
2023-05-11 00:19:42 -0400

    Update A_Explore_Preprocess_Data.py

--- pages/A_Explore_Preprocess_Data.py
+++ pages/A_Explore_Preprocess_Data.py
@@ -1,5 +1,5 @@
 import streamlit as st                  # pip install streamlit
-from helper_functions import  display_missingValue, fill_missingValue
+from helper_functions import  display_missingValue
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -66,12 +66,39 @@ if cab_data and weather_data is not None:
     missing_data = display_missingValue(df_cab, df_weather)
     st.dataframe(missing_data)
     # Deal with missing values for df_cab /Olga
-    
-    #display missing data for df_weather
-    
+    #remove missing data in df_cab
+    st.markdown('### Remove Missing df_cab Data')
+    #create a button to run in streamlit for remove missing data in df_cab
+    if st.button('Remove Missing df_cab Data'):
+        df_cab = df_cab.dropna()
+        st.write(df_cab)
+        #write df_cab has removed missing data
+        st.write('df_cab has removed missing data')
 
-    #merge df_cab and df_weather /Mary
+    # Deal with missing values for df_weather 
+    #fill missing data in df_weather
+    st.markdown('### Fill Missing df_weather Data')
+    #create a button to run in streamlit for fill missing data in df_weather with 0
+    if st.button('Fill Missing df_weather Data'):
+        df_weather = df_weather.fillna(0)
+        st.write(df_weather)
+        #write df_weather has filled missing data
+        st.write('df_weather has filled missing data')
+
+    
+    #drop time_stamp column in df_weather
+    st.markdown('### Drop df_weather Time_stamp Column')
+    df_weather = df_weather.drop(['time_stamp'], axis=1)
+    st.write(df_weather)
+    # group weather data by average based on the same location
+    st.markdown('### Group df_weather Data by average based on the same location')
+    df_weather_avg = df_weather.groupby(['location']).mean().reset_index()
+    st.write(df_weather_avg)
+
+    #merge df_cab and df_weather
     st.markdown('### Merge cab and weather data')
+    df = pd.merge(df_cab, df_weather_avg, on='location')
+    
 
 
     # Handle Text and Categorical Attributes

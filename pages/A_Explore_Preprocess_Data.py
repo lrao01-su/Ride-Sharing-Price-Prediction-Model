cd9af06fd911917e4ed454c68df9200406336a4d
lrao01-su <lrao01@syr.edu>
2023-05-10 21:40:49 -0400

    Revert "dropping missing values from cab and weather"
    This reverts commit 52f0cf8b72dd871cb844c9ff53fea1d35a73d1c1.

--- pages/A_Explore_Preprocess_Data.py
+++ pages/A_Explore_Preprocess_Data.py
@@ -3,6 +3,18 @@ from helper_functions import fetch_dataset, display_missingValue
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+from pandas.plotting import scatter_matrix
+import os
+import tarfile
+import urllib.request
+from itertools import combinations
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LinearRegression
+
 #############################################
 
 st.markdown("# Practical Applications of Machine Learning (PAML)")
@@ -38,7 +50,6 @@ df = fetch_dataset()
 =======
 >>>>>>> parent of c3c48af (Update A_Explore_Preprocess_Data.py)
 
-
 if df is not None:
     # Display original dataframe
     st.markdown('View initial data with missing values or invalid inputs')
@@ -56,8 +67,7 @@ if df is not None:
 
     #display missing data for df_cab and df_weather/ Olga
     st.markdown('### Missing Data') 
-    missing_data = display_missingValue(df_rides, df_weather)
-
+    missing_data = display_missingValue(df_cab, df_weather)
     st.dataframe(missing_data)
     # Deal with missing values for cab /Olga
     st.markdown('### Handle missing values for cab')
@@ -70,6 +80,8 @@ if df is not None:
 
     # Handle Text and Categorical Attributes
     st.markdown('### Handling Non-numerical Features')
+
+
 
     st.markdown('### You have preprocessed the dataset.')
     st.dataframe(df)

7d6bdb4cb88ca125fac1f1234f88257c9da49e50
Meet Oza <40018473+MEETOZA18@users.noreply.github.com>
2023-05-13 16:19:35 -0400

    Update B_Train_Model.py

--- pages/B_Train_Model.py
+++ pages/B_Train_Model.py
@@ -1,5 +1,8 @@
 import streamlit as st                  # pip install streamlit
-from helper_functions import fetch_dataset
+#from helper_functions import fetch_dataset
+from sklearn.model_selection import train_test_split
+
+from helper_functions import lasso_reg, linear_reg, random_forest, gradient_boost, one_hot_encode_feature
 
 #############################################
 
@@ -14,9 +17,51 @@ st.markdown("### Farelytics ML: Using Machine Learning to Analyze and Compare Ub
 st.title('Train Model')
 
 #############################################
-df = None
-df = fetch_dataset()
 
+df = st.session_state['data']
+
+def split_dataset(X, y, number,random_state=45):
+    """
+    This function splits the dataset into the train data and the test data
+
+    Input: 
+        - X: training features
+        - y: training targets
+        - number: the ratio of test samples
+    Output: 
+        - X_train: training features
+        - X_val: test/validation features
+        - y_train: training targets
+        - y_val: test/validation targets
+    """
+    X_train = []
+    X_val = []
+    y_train = []
+    y_val = []
+    
+
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=number/100, random_state=random_state)
+        
+
+    train_percentage = (len(X_train) /
+                            (len(X_train)+len(X_val)))*100
+
+    test_percentage = ((len(X_val))/
+                           (len(X_train)+len(X_val)))*100
+
+
+    # Print dataset split result
+    st.markdown('The training dataset contains {0:.2f} observations ({1:.2f}%) and the test dataset contains {2:.2f} observations ({3:.2f}%).'.format(len(X_train),
+                                                                                                                                                          train_percentage,
+                                                                                                                                                          len(X_val),
+                                                                                                                                                          test_percentage))
+    # Save state of train and test splits in st.session_state
+    st.session_state['X_train'] = X_train
+    st.session_state['X_val'] = X_val
+    st.session_state['y_train'] = y_train
+    st.session_state['y_val'] = y_val
+
+    return X_train, X_val, y_train, y_val
 
 def inspect_coefficients(models):
     pass
@@ -25,25 +70,48 @@ def inspect_coefficients(models):
 if df is not None:
     # Display dataframe as table
     st.dataframe(df)
+    string_columns = list(df.select_dtypes(['object']).columns)
 
-    # Select variable to predict
-    st.markdown('### Select variable to predict')
+    text_feature_select_onehot = st.selectbox('Select text features for One-hot encoding', string_columns)
+
+    df = df.dropna()
+
+    if (text_feature_select_onehot and st.button('One-hot Encode feature')):
+        if 'one_hot_encode' not in st.session_state:
+            st.session_state['one_hot_encode'] = {}
+        if text_feature_select_onehot not in st.session_state['one_hot_encode']:
+            st.session_state['one_hot_encode'][text_feature_select_onehot] = True
+        else:
+            st.session_state['one_hot_encode'][text_feature_select_onehot] = True
+        df = one_hot_encode_feature(df, text_feature_select_onehot)
+
     feature_predict_select = st.selectbox(
         label='Select variable to predict',
-        options=df.columns,
+        options=list(df.select_dtypes(include='number').columns),
         key='feature_selectbox',
+        index=8
     )
 
+    st.session_state['target'] = feature_predict_select
+
     # Select input features
-    st.markdown('### Select input features')
-    feature_input_select = st.selectbox(
-        label='Select features for classification input',
-        options=df.columns,
-        key='feature_select'
+    feature_input_select = st.multiselect(
+        label='Select features for regression input',
+        options=[f for f in list(df.select_dtypes(
+            include='number').columns) if f != feature_predict_select],
+        key='feature_multiselect'
     )
+
+    st.session_state['feature'] = feature_input_select
 
     st.write('You selected input {} and output {}'.format(
         feature_input_select, feature_predict_select))
+
+
+    X = df.loc[:, df.columns.isin(feature_input_select)]
+    Y = df.loc[:, df.columns.isin([feature_predict_select])]
+
+
 
     # Split dataset
     st.markdown('### Split dataset into Train/Validation/Test sets')
@@ -52,85 +120,177 @@ if df is not None:
     number = st.number_input(
         label='Enter size of test set (X%)', min_value=0, max_value=100, value=30, step=1)
 
-    # Train models
-    st.markdown('### Train models')
-    model_options = ['Placehoder']
+
+    X_train, X_val, y_train, y_val = split_dataset(X, Y, number)
+
+    st.write(len(X_train))
+    st.write(len(X_val))
+
+    regression_methods_options = ['Linear Regression',
+                                  'Lasso Regression', 'Random Forest', 'Gradient Boosting']
 
     # Collect ML Models of interests
-    model_select = st.multiselect(
+    regression_model_select = st.multiselect(
         label='Select regression model for prediction',
-        options=model_options,
+        options=regression_methods_options,
     )
-    st.write('You selected the follow models: {}'.format(model_select))
+    st.write('You selected the follow models: {}'.format(
+        regression_model_select))
 
-    if (model_options[0] in model_select):
-        st.markdown('#### ' + model_options[0])
 
-        param_col1, param_col2 = st.columns(2)
-        with (param_col1):
-            param1_options = []
-            param1_select = st.selectbox(
-                label='Select param1',
-                options=param1_options,
-                key='param1_select'
-            )
-            st.write('You select the following <param1>: {}'.format(param1_select))
 
-            param2_options = []
-            param2_select = st.selectbox(
-                label='Select param2',
-                options=param2_options,
-                key='param2_select'
-            )
-            st.write('You select the following <param2>: {}'.format(param2_select))
+    #  Linear Regression
 
-        with (param_col2):
-            param3_options = []
-            param3_select = st.selectbox(
-                label='Select param3',
-                options=param3_options,
-                key='param3_select'
-            )
-            st.write('You select the following <param3>: {}'.format(param3_select))
 
-            param4_options = []
-            param4_select = st.selectbox(
-                label='Select param4',
-                options=param4_options,
-                key='param4_select'
-            )
-            st.write('You select the following <param4>: {}'.format(param4_select))
+    if (regression_methods_options[0] in regression_model_select):
+        st.markdown('#### ' + regression_methods_options[0])
 
-        model_params = {
-            'param1': param1_select,
-            'param2': param2_select,
-            'param3': param3_select,
-            'param4': param4_select
-        }
+        cv_linear = st.number_input(
+            label='Enter the Cross Validation',
+            min_value=0,
+            max_value=10,
+            value=3,
+            step=1,
+            key='cv_linear'
+        )
 
-        if st.button('Placeholder Model'):
-            # train this model
-            pass
+        st.write('You set the CV to: {}'.format(cv_linear))
 
-        if model_options[0] not in st.session_state:
-            st.write('Placeholder Model is untrained')
+        
+        if st.button('Train Linear Regression Model'):
+            linear_reg(
+                X_train, y_train, cv_linear)
+
+        if regression_methods_options[0] not in st.session_state:
+            st.write('Linear Regression Model is untrained')
         else:
-            st.write('Placeholder Model trained')
+            st.write('Linear Regression Model trained')
 
-    # Inspect coefficients
-    st.markdown('### Inspect model coefficients')
+    #  Lasso Regression
 
-    inspect_models = st.multiselect(
-        label='Select models to inspect coefficients',
-        options=model_select,
-        key='inspect_multiselect'
-    )
+    if (regression_methods_options[1] in regression_model_select):
+        st.markdown('#### ' + regression_methods_options[1])
 
-    models = {}
-    for model_name in inspect_models:
-        if (model_name in st.session_state):
-            models[model_name] = st.session_state[model_name]
+        cv_lasso = st.number_input(
+            label='Enter the Cross Validation',
+            min_value=0,
+            max_value=10,
+            value=3,
+            step=1,
+            key='cv_lasso'
+        )
 
-    coefficients = inspect_coefficients(models)
+        st.write('You set the CV to: {}'.format(cv_lasso))
+
+        lasso_alpha = st.text_input(
+                label='Input alpha values, separate by comma',
+                value='0.001,0.0001',
+                key='lasso_alpha'
+            )
+        lasso_alpha = [float(val) for val in lasso_alpha.split(',')]
+
+        st.write('You select the following alpha value: {}'.format(lasso_alpha))
+
+
+        
+        if st.button('Train Lasso Regression Model'):
+            lasso_reg(X_train, y_train, cv_lasso, lasso_alpha)
+
+        if regression_methods_options[1] not in st.session_state:
+            st.write('Lasso Regression Model is untrained')
+        else:
+            st.write('Lasso Model trained')
+
+    #  Random Forest
+
+    if (regression_methods_options[2] in regression_model_select):
+        st.markdown('#### ' + regression_methods_options[2])
+
+        cv_random_forest = st.number_input(
+            label='Enter the Cross Validation',
+            min_value=0,
+            max_value=10,
+            value=3,
+            step=1,
+            key='cv_random_forest'
+        )
+
+        st.write('You set the CV to: {}'.format(cv_random_forest))
+
+        estimator_range = st.text_input(
+                label='Input estimator range',
+                value='100, 300',
+                key='estimator_range'
+            )
+
+        estimator_range = [int(val) for val in estimator_range.split(',')]
+
+        st.write('You select the following estimator range value: {}'.format(estimator_range))
+
+        max_depth = st.text_input(
+                label='Input max_depth',
+                value='5, 10',
+                key='max_depth'
+            )
+
+        max_depth = [int(val) for val in max_depth.split(',')]
+
+        st.write('You select the following max_depth value: {}'.format(max_depth))
+
+        
+        if st.button('Train Random Forest Model'):
+            random_forest(X_train, y_train, cv_random_forest, estimator_range, max_depth)
+
+        if regression_methods_options[2] not in st.session_state:
+            st.write('Random Forest Model is untrained')
+        else:
+            st.write('Random Forest Model trained')
+
+
+    #  Gradient Boosting
+
+    if (regression_methods_options[3] in regression_model_select):
+        st.markdown('#### ' + regression_methods_options[3])
+
+        cv_gb = st.number_input(
+            label='Enter the Cross Validation',
+            min_value=0,
+            max_value=10,
+            value=3,
+            step=1,
+            key='cv_gb'
+        )
+
+        st.write('You set the CV to: {}'.format(cv_gb))
+
+        estimator_range_gb = st.text_input(
+                label='Input estimator range',
+                value='100, 300',
+                key='estimator_range_gb'
+            )
+
+        estimator_range_gb = [int(val) for val in estimator_range_gb.split(',')]
+
+        st.write('You select the following estimator range value: {}'.format(estimator_range_gb))
+
+        lr = st.text_input(
+                label='Input learning rate',
+                value='0.01, 0.1',
+                key='lr'
+            )
+
+        lr = [float(val) for val in lr.split(',')]
+
+        st.write('You select the following estimator range value: {}'.format(lr))
+
+        
+        if st.button('Train Gradient Boosting Model'):
+            gradient_boost(X_train, y_train, cv_random_forest, estimator_range_gb, lr)
+
+        if regression_methods_options[3] not in st.session_state:
+            st.write('Gradient Boosting Model is untrained')
+        else:
+            st.write('Gradient Boosting Model trained')
+
 
     st.write('Continue to Test Model')

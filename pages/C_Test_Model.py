0676d58fd38d015fccbee8fa6d50961071831c80
lrao01-su <lrao01@syr.edu>
2023-05-14 13:33:49 -0400

    Update C_Test_Model.py

--- pages/C_Test_Model.py
+++ pages/C_Test_Model.py
@@ -1,5 +1,10 @@
 import streamlit as st                  # pip install streamlit
-from helper_functions import fetch_dataset
+import random
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from helper_functions import rmse, mae, r2, split_dataset, compute_eval_metrics, plot_learning_curve
+import numpy as np
+import pandas as pd
 
 #############################################
 
@@ -7,7 +12,7 @@ st.markdown("# Practical Applications of Machine Learning (PAML)")
 
 #############################################
 
-st.markdown("### Final Project - <project title>")
+st.markdown("### Final Project -  Uber Lyft Cab Price Prediction")
 
 #############################################
 
@@ -15,16 +20,83 @@ st.title('Test Model')
 
 #############################################
 
-df = None
-df = fetch_dataset()
+METRICS_MAP = {
+    'mean_absolute_error': mae,
+    'root_mean_squared_error': rmse,
+    'r2_score': r2
+}
+
+# Page C
+def restore_data_splits(df):
+    """
+    This function restores the training and validation/test datasets from the training page using st.session_state
+                Note: if the datasets do not exist, re-split using the input df
+
+    Input: 
+        - df: the pandas dataframe
+    Output: 
+        - X_train: the training features
+        - X_val: the validation/test features
+        - y_train: the training targets
+        - y_val: the validation/test targets
+    """
+    X_train = None
+    y_train = None
+    X_val = None
+    y_val = None
+    # Restore train/test dataset
+    if ('X_train' in st.session_state):
+        X_train = st.session_state['X_train']
+        y_train = st.session_state['y_train']
+        st.write('Restored train data ...')
+    if ('X_val' in st.session_state):
+        X_val = st.session_state['X_val']
+        y_val = st.session_state['y_val']
+        st.write('Restored test data ...')
+    if (X_train is None):
+        # Select variable to explore
+        numeric_columns = list(df.select_dtypes(include='number').columns)
+        feature_select = st.selectbox(
+            label='Select variable to predict',
+            options=numeric_columns,
+        )
+        X = df.loc[:, ~df.columns.isin([feature_select])]
+        Y = df.loc[:, df.columns.isin([feature_select])]
+
+        # Split train/test
+        st.markdown(
+            '### Enter the percentage of test data to use for training the model')
+        number = st.number_input(
+            label='Enter size of test set (X%)', min_value=0, max_value=100, value=30, step=1)
+
+        X_train, X_val, y_train, y_val = split_dataset(X, Y, number)
+        st.write('Restored training and test data ...')
+    return X_train, X_val, y_train, y_val
+
+df = st.session_state['data']
 
 if df is not None:
+    X_train, X_val, y_train, y_val = restore_data_splits(df)
     st.markdown("### Get Performance Metrics")
-    metric_options = ['placeholder']
+    metric_options = ['mean_absolute_error',
+                      'root_mean_squared_error', 'r2_score']
+    
+    # Select multiple metrics for evaluation
+    metric_select = st.multiselect(
+        label='Select metrics for regression model evaluation',
+        options=metric_options,
+    )
+    if (metric_select):
+        st.session_state['metric_select'] = metric_select
+        st.write('You selected the following metrics: {}'.format(metric_select))
 
-    model_options = ['placeholder']
+    regression_methods_options = ['Linear Regression',
+                                  'Lasso Regression', 'Random Forest', 'Gradient Boosting']
 
-    trained_models = ['placeholder']
+
+    trained_models = [
+        model for model in regression_methods_options if model in st.session_state]
+    st.session_state['trained_models'] = trained_models
 
     # Select a trained classification model for evaluation
     model_select = st.multiselect(
@@ -42,20 +114,44 @@ if df is not None:
             st.session_state['eval_button_clicked'] = eval_button
 
         if 'eval_button_clicked' in st.session_state and st.session_state['eval_button_clicked']:
-            st.markdown('### Review Model Performance')
+            st.markdown('## Review Regression Model Performance')
 
-            review_options = ['plot', 'metrics']
+            review_model_select = st.multiselect(
+                label='Select models to review',
+                options=model_select
+            )
+
+            plot_options = ['Learning Curve', 'Metric Results']
 
             review_plot = st.multiselect(
                 label='Select plot option(s)',
-                options=review_options
+                options=plot_options
             )
 
-            if 'plot' in review_plot:
-                pass
+            if 'Learning Curve' in review_plot:
+                for model in review_model_select:
+                    trained_model = st.session_state[model]
+                    fig, df = plot_learning_curve(
+                        X_train, X_val, y_train, y_val, trained_model, metric_select, model)
+                    st.plotly_chart(fig)
 
-            if 'metrics' in review_plot:
-                pass
+            if 'Metric Results' in review_plot:
+                models = [st.session_state[model]
+                          for model in review_model_select]
+
+                train_result_dict = {}
+                val_result_dict = {}
+                for idx, model in enumerate(models):
+                    train_result_dict[review_model_select[idx]] = compute_eval_metrics(
+                        X_train, y_train, model, metric_select)
+                    val_result_dict[review_model_select[idx]] = compute_eval_metrics(
+                        X_val, y_val, model, metric_select)
+
+                st.markdown('### Predictions on the training dataset')
+                st.dataframe(train_result_dict)
+
+                st.markdown('### Predictions on the validation dataset')
+                st.dataframe(val_result_dict)
 
     # Select a model to deploy from the trained models
     st.markdown("### Choose your Deployment Model")

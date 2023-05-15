b98de3c0b1b8f97825a9e11d933d39962bdf7bc3
lrao01-su <lrao01@syr.edu>
2023-05-14 21:36:09 -0400

    Create E_Summary.py

---
+++ pages/E_Summary.py
@@ -0,0 +1,13 @@
+import streamlit as st              # pip install streamlit
+
+#write section title
+st.markdown("# Project Summary")
+st.markdown("### Farelytics ML: Using Machine Learning to Analyze and Compare Uber and Lyft Pricing Models")
+
+#############################################
+
+#############################################
+
+#############################################
+
+
